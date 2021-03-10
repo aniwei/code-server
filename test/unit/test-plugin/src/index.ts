668e56e2d831f5243102833ac653703bf12186e4
Joe Previte <jjprevite@gmail.com>
2021-03-10 11:56:12 -0700

    chore: fix typo in eslint rc file

--- test/unit/test-plugin/src/index.ts
+++ test/unit/test-plugin/src/index.ts
@@ -1,22 +1,24 @@
 import * as cs from "code-server"
 import * as fspath from "path"
 
+type FixMeLater = any
+
 export const plugin: cs.Plugin = {
   displayName: "Test Plugin",
   routerPath: "/test-plugin",
   homepageURL: "https://example.com",
   description: "Plugin used in code-server tests.",
 
-  init(config) {
+  init(config: FixMeLater) {
     config.logger.debug("test-plugin loaded!")
   },
 
   router() {
     const r = cs.express.Router()
-    r.get("/test-app", (_, res) => {
+    r.get("/test-app", (_: FixMeLater, res: FixMeLater) => {
       res.sendFile(fspath.resolve(__dirname, "../public/index.html"))
     })
-    r.get("/goland/icon.svg", (_, res) => {
+    r.get("/goland/icon.svg", (_: FixMeLater, res: FixMeLater) => {
       res.sendFile(fspath.resolve(__dirname, "../public/icon.svg"))
     })
     r.get("/error", () => {
@@ -27,8 +29,8 @@ export const plugin: cs.Plugin = {
 
   wsRouter() {
     const wr = cs.WsRouter()
-    wr.ws("/test-app", (req) => {
-      cs.wss.handleUpgrade(req, req.ws, req.head, (ws) => {
+    wr.ws("/test-app", (req: FixMeLater) => {
+      cs.wss.handleUpgrade(req, req.ws, req.head, (ws: FixMeLater) => {
         req.ws.resume()
         ws.send("hello")
       })
